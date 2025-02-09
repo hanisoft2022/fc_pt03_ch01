f775eb7683ee351e6b86da3d922f95074d7af210
hani <hanisoft2022@gmail.com>
2025-02-09 14:27:45 +0900

    splash screen configuring, ios fin, android not configed yet

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'package:shake_count_app/my_home_page.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // 테스트용 splash screen 유지, test 완료 후 아래 코드 제거
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
